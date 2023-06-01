69ad68837d9ff06f160cdd7c3dda355bde967197
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-01 18:38:21 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,13 +62,13 @@ st.dataframe(df.head())
 with st.form("my_form"):
 
    query = st.text_input(label ="Enter a question" , placeholder = 'Enter your query')
-   cols_2_pass = openai_response(f""" Give output only in format as each expected column name seperated by comma like column1,column2,column3,....
+   cols_2_pass = openai_response(f""" Give output only in python list format as each expected column name seperated by comma like column1,column2,column3,....
                                  A dataframe with following column names : {df.columns}. 
                                  Find all column names which will be used in following query: {query}""")
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       st.write(cols_2_pass.split(','))
+       st.write(cols_2_pass)
  #      if contains_substring(query.lower(),ls): 
  #       fig, x = plt.subplots()
  #       response = pandas_ai(df, prompt=query)
