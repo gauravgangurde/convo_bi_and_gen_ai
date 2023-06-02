ae9c905ef903d784509e4038b51718af8387f0b8
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-02 15:48:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ with tab2:
 			)
 	if response2 != '':
 		st.download_button('Download text', response2)
-					st.download_button(
+		st.download_button(
 			    label="Download data as CSV",
 			    data='employees.csv',
 			    file_name='large_df.csv',
