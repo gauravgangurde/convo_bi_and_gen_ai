2e335b38f03937f6ec6a1c1720a58497a4f6e2ff
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-02 14:48:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,78 +7,106 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import openai
 
-#st.set_option('deprecation.showPyplotGlobalUse', False)
+#EXL logo
 image = Image.open('exl.png')
 
+tab1, tab2 = st.tabs(['Analysis','Communication'])
 
-llm = OpenAI(api_token=st.secrets["chat_gpt_key"])
+with tab1:
+	
+	llm = OpenAI(api_token=st.secrets["chat_gpt_key"])
+	
+	pandas_ai = PandasAI(llm, conversational=False, enforce_privacy = True)
+	
+	df1 = pd.read_csv('employees.csv')
+	df2 = pd.read_csv('sales_data.csv')
+	df3 = pd.read_csv('claims_data.csv')
+	
+	ls = ['chart','plot','graph','trend']
+	#to check if prompt have chart, graph words
+	def contains_substring(string, substrings):
+		for substring in substrings:
+			if substring in string:
+				return True
+		return False
+		
+		
+	with st.sidebar:
+		st.image(image, width = 150)
+		st.header('Conversational BI')
+		st.write('Ask any question on your BI report')
+		st.write(' ')
+		st.write(' ')
+		role = st.selectbox('Please select your role',('HR Manager', 'Sales Manager', 'Claims Manager'))
+	
+	
+	#based on role selected show BI report  
+	if role == 'HR Manager':
+		df = df1
+	elif role == 'Sales Manager':
+		df = df2
+	elif role == 'Claims Manager':
+		df = df3
+	
+	st.header("BI Report (Structure): " + role.replace('Manager',''))
+	st.dataframe(df.head())
+	
+	with st.form("my_form"):
+		
+		query = st.text_input(label ="Enter a question" , placeholder = 'Enter your query')
+		# Every form must have a submit button.
+		submitted = st.form_submit_button("Submit")
+		if submitted:
+			if contains_substring(query.lower(),ls): 
+				fig, x = plt.subplots()
+				response = pandas_ai(df, prompt=query)
+				st.pyplot(fig)
+				st.text(response)
+			else:
+				response = pandas_ai(df, prompt=query)
+				st.text(response)
+				
 
-pandas_ai = PandasAI(llm, conversational=False, enforce_privacy = True)
 
-df1 = pd.read_csv('employees.csv')
-df2 = pd.read_csv('sales_data.csv')
-df3 = pd.read_csv('claims_data.csv')
+with tab2:
 
-ls = ['chart','plot','graph','trend']
-#to check if prompt have chart, graph words
-def contains_substring(string, substrings):
-	for substring in substrings:
-		if substring in string:
-			return True
-	return False
+openai.api_key = st.secrets["chat_gpt_key"]
 
-#query to open AI
+df = pd.read_csv('performance.csv')
+
 def openai_response(query):
 	response = openai.ChatCompletion.create(
 	model="gpt-3.5-turbo",
-	temperature = 0,
-
 	messages = [
 		{"role":"system", "content":"You are helpful assistant."},
 		{"role":"user","content": query}
 	]
-   )
-	return response.choices[0]['message']['content']
+	)
+	return response.choices[0]['message']['content']   
     
 with st.sidebar:
+
 	st.image(image, width = 150)
-	st.header('Conversational BI')
-	st.write('Ask any question on your BI report')
-	st.write(' ')
-	st.write(' ')
-	role = st.selectbox('Please select your role',('HR Manager', 'Sales Manager', 'Claims Manager'))
-
-
-#based on role selected show BI report  
-if role == 'HR Manager':
-	df = df1
-elif role == 'Sales Manager':
-	df = df2
-elif role == 'Claims Manager':
-	df = df3
-
-st.header("BI Report (Structure): " + role.replace('Manager',''))
-st.dataframe(df.head())
-
-with st.form("my_form"):
-	cols = []
-	query = st.text_input(label ="Enter a question" , placeholder = 'Enter your query')
-	cols_2_pass = openai_response(f"""Please only give output as follows, with the names of each expected column separated by commas without space and dots such as column1,column2,column3
-                                 A dataframe with following column names : {df.columns}
-                                 Find all column names which will be used in following query: {query}""").strip('.').split(',')
-   # Every form must have a submit button.
-	submitted = st.form_submit_button("Submit")
-	if submitted:
-		for item in cols_2_pass:
-			cols.append(item.strip())
-		st.write(cols)
-		st.dataframe(df[cols].head())
-		if contains_substring(query.lower(),ls): 
-			fig, x = plt.subplots()
-			response = pandas_ai(df[cols], prompt=query)
-			st.pyplot(fig)
-			st.text(response)
-		else:
-			response = pandas_ai(df[cols], prompt=query)
-			st.text(response)
-
+	st.header('Generative AI')
+	
+	
+	st.header("Personalized communication ")
+	
+	with st.form("my_form"):
+		name = st.selectbox('Please select name',df["name"])
+		intent_of_mail = st.text_input(label ="Intent of mail" , placeholder = 'Intent')
+		category = df[df.name == name]['performance'].to_string(index=False)
+		target = df[df.name == name]['target'].to_string(index=False)
+		latest_performance = df[df.name == name]['latest_month_performance'].to_string(index=False)
+		
+		# Every form must have a submit button.
+		submitted = st.form_submit_button("Submit")
+		if submitted:
+			response = openai_response(f"""Write a {intent_of_mail} mail to a salesperson {name} as their employer based on following information starts and ends with triplle dashes marks,
+	Analyse the data to determine whether a salesperson's performance is above or below target and how it impacts the performance category,
+	offer some insight based on performance and their category,
+	--- {name} is {category} with their target, their latest target was {target} and latest performance was {latest_performance} ---
+	""")
+		st.text(f"""Category : {category}\nTarget : {target}\nLatest performance : {latest_performance}""")
+		st.write()
+		st.markdown(response)
