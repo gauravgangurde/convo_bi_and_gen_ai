379177523e1e0f035c48c7eaf8896c1bf64d2513
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-05 13:36:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,10 +86,9 @@ with tab2:
 		if submitted2:
 			response2 = openai_response(f"""Your task is to write appraisal mail to insurance agent analysing their performance,
 					agent data is delimited by triple backticks,
-					Analyse given data first then include below to do if condition met
-					1) condition >> performance below target. to do >> add references to sales improvement articles and tarining links
-					2) condition >> sales growth is in negative. to do >> point that out and add some insights
-					Then write detail mail and sign mail as 'Manager'
+					1)Analyse given data
+					2) Mail should include sales improvement article or training link references based on performance category
+					3)sign mail as 'Manager'
 					data : ```{name} is {category} with their target, their latest target was {target} and latest performance was {latest_performance}
 					 and their total sales growth with respective previous month is {growth}```
 					""")
