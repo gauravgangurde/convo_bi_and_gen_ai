16e0edd58857b06eee917cf07b8ebc7a53e7a8cd
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-05 13:22:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,12 +86,14 @@ with tab2:
 		if submitted2:
 			response2 = openai_response(f"""Your task is to write appraisal mail to insurance agent analysing their performance,
 					agent data is delimited by triple backticks,
-					```{name} is {category} with their target, their latest target was {target} and latest performance was {latest_performance} and their total sales growth is {growth}```
-					if agents current sales performnace is below target, provide sales insight and offer sales improvement exercise, articles and training reference links in email body
-					if agents current sales performance is above target, applaud them for their achievemnet
-					if sales growth is negative then give some insight for improvement
-					if sales growth is positive then congratulate them and
+					analyse data and draft mail with below point, according to it
+					1)if agents current sales performnace is below target, provide sales insight and offer sales improvement exercise, articles and training reference links in email body
+					2)if agents current sales performance is above target, applaud them for their achievemnet
+					3)if sales growth is negative then give some insight for improvement
+					4)if sales growth is positive then congratulate them and
 					And sign mail as 'Manager'
+					
+					data : ```{name} is {category} with their target, their latest target was {target} and latest performance was {latest_performance} and their total sales growth is {growth}```
 					""")
 			st.text(f"""Name: {name}\nCategory : {category}\nTarget : {target}\nLatest performance : {latest_performance}\nSales growth: {growth}""")
 			st.write()
