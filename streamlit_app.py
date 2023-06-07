8581f89c123473288b87d21e9b40540d5206499e
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-07 20:02:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,15 +118,15 @@ with tab3:
 		st.text(f"""Name: {name}\nCategory : {category}\nTarget : ${target}\nCurrnt Sales : ${curr_sales}""")
 		st.write()
 		mail_response = ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value
-		st.markdown(mail_response)
+		#st.markdown(mail_response)
 	
-		if st.button('Edit', key = 'ABC'):
-			#name_index = df2.loc[df2.Name == name].index[0] + 2
-			mail_index = 10
-			user_input = st.text_area("Edit the mail",height = 600, value= ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value)
-			submitted3 = st.button("Validate")
-			if submitted3:
-				ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value = user_input
-				st.write("Message updated successfully")
-				st.markdown(ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value)
+		#if st.button('Edit', key = 'ABC'):
+		#name_index = df2.loc[df2.Name == name].index[0] + 2
+		mail_index = 10
+		user_input = st.text_area("Edit the mail",height = 600, value= ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value)
+		submitted3 = st.button("Validate")
+		if submitted3:
+			ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value = user_input
+			st.write("Message updated successfully")
+			st.markdown(ws.cell(row = df2.loc[df2.Name == name].index[0] + 2 , column = mail_index).value)
 					
