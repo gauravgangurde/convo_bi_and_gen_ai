5b247215719d6d43bf5f8651c672769a1ed55d8e
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-07 16:34:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,18 +121,29 @@ with tab3:
 		category = df2[df2.Name == name]['Category'].to_string(index=False)
 		target = df2[df2.Name == name]['Target'].to_string(index=False)
 		curr_sales = df2[df2.Name == name]['Sales'].to_string(index=False)
+		
+		# row and column index of required obs
+		name_index = ws.cell(row = df.loc[df.Name == name].index[0] + 2
+		mail_index = 10
 		# Every form must have a submit button.
 		submitted2 = st.form_submit_button("Validate")
 		if submitted2:
-			
-
 			st.text(f"""Name: {name}\nCategory : {category}\nTarget : ${target}\nCurrnt Sales : ${curr_sales}""")
 			st.write()
-			mail_response = ws.cell(row = df.loc[df.Name == name].index[0] + 2, column = 10).value
+			mail_response = ws.cell(row = name_index , column = mail_index).value
 			st.markdown(mail_response)
 	#if "mail_response" in globals():
 	#	st.write('Please select name first')
 	#else:
 	if st.button('Edit', key = 'ABC'):
-		user_input = st.text_area("Edit the mail",height = 1000, value= ws.cell(row = df.loc[df.Name == name].index[0] + 2, column = 10).value)
-			
+		if not name:
+			st.write('Please select name first')
+		else:
+			with st.form("edit communication"):
+				name_index = ws.cell(row = df.loc[df.Name == name].index[0] + 2
+				mail_index = 10
+				user_input = st.text_area("Edit the mail",height = 600, value= ws.cell(row = name_index , column = mail_index).value)
+				submitted3 = st.form_submit_button("Save")
+				if submitted3:
+					 ws.cell(row = name_index , column = mail_index).value = user_input
+					
