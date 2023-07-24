8f875ec7926bfe57ff1dffa580017fdcaedbd796
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-24 15:37:14 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -12,7 +12,7 @@ import openpyxl
 image = Image.open('exl.png')
 #read data file in dataframe
 #df = pd.read_csv('data.csv')
-df = pd.read_exel('Mort.xlsx')
+df = pd.read_excel('Mort.xlsx')
 
 with st.sidebar:
 	st.image(image, width = 150)
