237bfd29158126537ed3c12deb20e78ff821ee1b
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 16:29:26 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -88,7 +88,7 @@ if st.button("Submit"):
 		df_out = pivot2(df,'UW Class')
 		title = 'Mortality experience by UW Class'
 
-	st.write(title)
+	st.text(title)
 	st.dataframe(df_out)
 	df_out.reset_index(level=0, inplace=True)
 
