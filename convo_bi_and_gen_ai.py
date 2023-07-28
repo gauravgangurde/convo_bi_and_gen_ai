611d3876070347502e355ec5d95ba7b37b23e1f7
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 18:20:17 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -73,7 +73,6 @@ if st.button("Submit"):
 		
 		# Set the positions of the bars on the x-axis
 		x = range(len(x_labels))
-		
 		# Plot the bar graph for each value of Term and Endowment
 		fig, ax = plt.subplots()
 		bar_width = 0.35
@@ -83,18 +82,41 @@ if st.button("Submit"):
 		# Label the axes and add a title
 		ax.set_xlabel('Duration')
 		ax.set_ylabel('Mortality')
-		ax.set_title('Endowment and Term Percentage')
-		
+		ax.set_title('Mortality experience by Product and Duration')
 		# Set the x-axis labels
 		ax.set_xticks([pos + bar_width / 2 for pos in x])
 		ax.set_xticklabels(x_labels)
-		
 		# Show the legend
 		ax.legend()
 		fig.savefig('Graph1.png')
+		
 	elif query == 'show mortality experience analysis by product and smoker status':
 		df_out = pivot1(df,'Product', 'Smoker Status')
 		title = 'Mortality experience by Product and Smoker Status'
+		chart = 'y'
+		df_t = df_out.set_index('Product/Smoker Status').T.reset_index()
+		df_t['Endowment'] = pd.to_numeric(df_t['Endowment'].str.strip('%').replace('nan',0))
+		df_t['Term'] = pd.to_numeric(df_t['Term'].str.strip('%').replace('nan',0))
+		x_labels = df_t['Smoker Status'].tolist()
+		
+		# Set the positions of the bars on the x-axis
+		x = range(len(x_labels))
+		# Plot the bar graph for each value of Term and Endowment
+		fig, ax = plt.subplots()
+		bar_width = 0.35
+		for i, label in enumerate(['Endowment', 'Term']):
+			ax.bar([pos + bar_width * i for pos in x], df_t[label], bar_width, label=label)
+	
+		# Label the axes and add a title
+		ax.set_xlabel('Smoker Status')
+		ax.set_ylabel('Mortality')
+		ax.set_title('Mortality experience by Product and Smoker Status')
+		# Set the x-axis labels
+		ax.set_xticks([pos + bar_width / 2 for pos in x])
+		ax.set_xticklabels(x_labels)
+		# Show the legend
+		ax.legend()
+		fig.savefig('Graph1.png')
 		
 	elif query == 'show mortality experience analysis by sum assured class and product':
 		df_out = pivot1(df,'Sum Assured Class', 'Product')
