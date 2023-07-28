01b0f2a4220153e5ada623b8787edc28446ef07f
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 17:14:46 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -107,7 +107,7 @@ if st.button("Submit"):
 		title = 'Mortality experience sum assured Class and Product'
 	elif query == 'show mortality experience analysis by issue year':
 		df_out = pivot2(df,'Issue Year')
-		df_out['Issue Year'] = pd.to_numeric(df_out['Issue Year'], errors='coerce').astype('Int64')
+		df_out['Issue Year'] = df_out['Issue Year'].astype(int)
 		title = 'Mortality experience by Issue Year'
 	elif query == 'show mortality experience analysis by uw class':
 		df_out = pivot2(df,'UW Class')
