092c804fea7e4dd254c85f018e113857a89ea67e
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 14:07:15 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -5,6 +5,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
 
 #EXL logo
 image = Image.open('exl.png')
@@ -61,16 +64,48 @@ inp_query = st.text_input(label ="Enter a question" , placeholder = 'Enter your 
 query = query_mapper(inp_query.lower())
 #st.subheader(query)
 
+def df_to_pdf(df, output_file):
+	# Convert DataFrame to a list of lists
+	data = [df.columns.tolist()] + df.values.tolist()
+	
+	# Create a PDF document
+	doc = SimpleDocTemplate(output_file, pagesize=letter)
+	
+	# Create a table from the DataFrame data
+	table = Table(data)
+	
+	# Define table style
+	style = TableStyle([
+		('BACKGROUND', (0, 0), (-1, 0), colors.grey),  # Header background color
+		('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),  # Header text color
+		('ALIGN', (0, 0), (-1, -1), 'CENTER'),  # Center-align all cells
+		('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),  # Header font
+		('BOTTOMPADDING', (0, 0), (-1, 0), 12),  # Header bottom padding
+		('BACKGROUND', (0, 1), (-1, -1), colors.beige),  # Table body background color
+		('GRID', (0, 0), (-1, -1), 1, colors.black),  # Table grid color
+		('FONTSIZE', (0, 1), (-1, -1), 10),  # Table font size
+	])
+	
+	# Apply the style to the table
+	table.setStyle(style)
+	
+	# Add table to the document
+	doc.build([table])
 
+
+# Usage example
+output_pdf_file = 'output_dataframe.pdf'
+df_to_pdf(pivot_table, output_pdf_file)
+print(f"DataFrame written to {output_pdf_file}.")
+
+with open("output_dataframe.pdf", "rb") as file:
+	st.download_button(
+		label="Download data",
+		data=file,
+		file_name='report.pdf'
+	)
 #submitted1 = st.form_submit_button("Submit")
 if st.button("Submit"):
-	
-	st.dataframe(response_df)
-	workbook = Workbook()
-	sheet = workbook.active
-	for row in dataframe_to_rows(response_df, index=False):
-		sheet.append(row)
-	workbook.save('output.xlsx')
 	with open("output.xlsx", "rb") as file:
 		st.download_button(
 			label="Download data",
