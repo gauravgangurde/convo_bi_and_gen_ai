c6479a08d502cb4773963457a2e1b55c56765571
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 18:38:19 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -152,8 +152,8 @@ if st.button("Submit"):
 		title = 'Mortality experience by Issue Year'
 		chart = 'y'
 		fig, ax = plt.subplots()
-		x = pd.to_numeric(df_t['Issue_Year'].str.strip('%').replace('nan',0))
-		y = pd.to_numeric(df_t['Mortality'].str.strip('%').replace('nan',0))
+		x = pd.to_numeric(df_out['Issue_Year'].str.strip('%').replace('nan',0))
+		y = pd.to_numeric(df_out['Mortality'].str.strip('%').replace('nan',0))
 		plt.plot(x, y, marker='o', linestyle='-')
 		ax.set_xlabel('Year')
 		ax.set_ylabel('Mortality')
