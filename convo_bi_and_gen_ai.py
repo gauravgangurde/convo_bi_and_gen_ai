ebfaa73a2c3917f9339563a5efc331a79e6ca99c
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 16:51:58 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -79,6 +79,41 @@ if st.button("Submit"):
 	if query == 'show mortality experience analysis by product and duration':
 		df_out = pivot1(df,'Product', 'Duration')
 		title = 'Mortality experience by Product and Duration'
+		fig, x= plt.subplots()
+		df_t = df_out.set_index('Product').T.reset_index()
+		
+		# Define the labels for the x-axis
+		x_labels = df_t['index'].tolist()
+		
+		# Set the positions of the bars on the x-axis
+		x = range(len(x_labels))
+		
+		# Plot the bar graph for each value of Term and Endowment
+		fig, ax = plt.subplots()
+		bar_width = 0.25
+		for i, label in enumerate(['Endowment', 'Term']):
+			ax.bar([pos + bar_width * i for pos in x], df_t[label], bar_width, label=label)
+		
+		# Label the axes and add a title
+		ax.set_xlabel('Duration')
+		ax.set_ylabel('Mortality')
+		ax.set_title('Endowment and Term Percentage')
+		
+		# Set the y-axis limit to match the percentage values (0 to 100)
+		ax.set_ylim(0, 100)
+		
+		# Set the x-axis labels
+		ax.set_xticks([pos + bar_width / 2 for pos in x])
+		ax.set_xticklabels(x_labels)
+		
+		# Show the legend
+		ax.legend()
+		
+		# Show the plot
+		plt.tight_layout()
+		plt.show()
+
+		fig.savefig('graph1.png')
 	elif query == 'show mortality experience analysis by product and smoker status':
 		df_out = pivot1(df,'Product', 'Smoker Status')
 		title = 'Mortality experience by Product and Smoker Status'
@@ -107,7 +142,7 @@ if st.button("Submit"):
 
 	#formatting graph
 	sheet2 = workbook.create_sheet(title='Graph')
-	graph = ii('exl.png')
+	graph = ii('graph1.png')
 	aspect_ratio = graph.width / graph.height
 	graph.width = 800
 	graph.height = graph.width/aspect_ratio
