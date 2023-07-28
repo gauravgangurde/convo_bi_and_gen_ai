cb7f830a489d52a5c1f959e494ff47a65743c42d
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-07-28 13:59:47 +0530

    Update convo_bi_and_gen_ai.py

--- convo_bi_and_gen_ai.py
+++ convo_bi_and_gen_ai.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI
 from PIL import Image
 import matplotlib.pyplot as plt
-import openai
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 
@@ -54,12 +51,6 @@ def query_mapper(query):
 	
 
 
-
-llm = OpenAI(api_token=st.secrets["chat_gpt_key"])
-
-pandas_ai = PandasAI(llm, conversational=False)#, enforce_privacy = True)
-
-
 st.subheader("Conversational BI")
 st.write("Sample data structure ")
 st.dataframe(df.head())
@@ -67,54 +58,24 @@ st.dataframe(df.head())
 #with st.form("conversation_bi"):
 
 inp_query = st.text_input(label ="Enter a question" , placeholder = 'Enter your query')
-#query = inp_query.lower().replace('mortality experience', 'percent of total actual death with respective total expected death')
-query = query_mapper(inp_query.lower()).replace('mortality experience', 'actual deaths divided by expected deaths in percent as Mortality')
+query = query_mapper(inp_query.lower())
 #st.subheader(query)
 
 
 #submitted1 = st.form_submit_button("Submit")
 if st.button("Submit"):
-	#based on type of response, check if user required graph/chart
-	if contains_substring(query.lower(),ls): 
-		fig, x = plt.subplots()
-		response1 = pandas_ai(df, prompt=query)
-		st.pyplot(fig)
-	else:
-		response1 = pandas_ai(df, prompt=query)
-		#check if output is in dataframe and download option
-		if isinstance(response1, pd.DataFrame):
-			st.dataframe(response1)
-			workbook = Workbook()
-			sheet = workbook.active
-			for row in dataframe_to_rows(response1):
-				sheet.append(row)
-			workbook.save('output.xlsx')
-			with open("output.xlsx", "rb") as file:
-				st.download_button(
-					label="Download data",
-					data=file,
-					file_name='data.xlsx'
-				)
-		#check if output is in tuple
-		elif isinstance(response1, tuple):
-			st.text(response1)
-		#check if output is in series, convert it to dataframe and download option
-		elif isinstance(response1, pd.Series):
-			#st.text(response1)
-			response_df = response1.to_frame().reset_index()
-			st.dataframe(response_df)
-			workbook = Workbook()
-			sheet = workbook.active
-			for row in dataframe_to_rows(response_df, index=False):
-				sheet.append(row)
-			workbook.save('output.xlsx')
-			with open("output.xlsx", "rb") as file:
-				st.download_button(
-					label="Download data",
-					data=file,
-					file_name='data.xlsx'
-				)
-		#for all other output formats
-		else:
-			st.text(response1)
+	
+	st.dataframe(response_df)
+	workbook = Workbook()
+	sheet = workbook.active
+	for row in dataframe_to_rows(response_df, index=False):
+		sheet.append(row)
+	workbook.save('output.xlsx')
+	with open("output.xlsx", "rb") as file:
+		st.download_button(
+			label="Download data",
+			data=file,
+			file_name='data.xlsx'
+		)
+
 		
